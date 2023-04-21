42cd96c002597f9ec8c2de212d08dcc4ad615722
Екатерина Савченко <kattysavchenko1@yandex.ru>
2023-04-21 16:17:55 +0200

    Спринт 12/18 → Тема 1/2: Основные структуры данных → Урок 8/14

---
+++ 02_basic_data_structures/unloved_business.py
@@ -0,0 +1,39 @@
+LOCAL = True
+
+if LOCAL:
+    class Node:
+        def __init__(self, value, next_item=None):
+            self.value = value
+            self.next_item = next_item
+
+
+def get_node_by_index(node, index):
+    while index:
+        node = node.next_item
+        index -= 1
+    return node
+
+
+def solution(head, idx):
+    next_node = get_node_by_index(head, idx+1)
+    if idx == 0:
+        return next_node
+    previous_node = get_node_by_index(head, idx-1)
+    previous_node.next_item = next_node
+    return head
+
+
+def test():
+    node3 = Node("node3", None)
+    node2 = Node("node2", node3)
+    node1 = Node("node1", node2)
+    node0 = Node("node0", node1)
+    new_head = solution(node0, 1)
+    assert new_head is node0
+    assert new_head.next_item is node2
+    assert new_head.next_item.next_item is node3
+    assert new_head.next_item.next_item.next_item is None
+
+
+if __name__ == '__main__':
+    test()
