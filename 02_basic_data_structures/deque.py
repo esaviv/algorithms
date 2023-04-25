a409079096bd7918c6f5fb1a6426a47977e55624
Екатерина Савченко <kattysavchenko1@yandex.ru>
2023-04-25 16:39:39 +0200

    Python-разработчик: основные структуры данных. Финальные задачи, A. Дек

--- 02_basic_data_structures/deque.py
+++ 02_basic_data_structures/deque.py
@@ -2,10 +2,8 @@ class Deque():
     def __init__(self, n) -> None:
         self.queue = [None] * n
         self.max_n = n
-        self.head1 = 0
-        self.tail1 = 0
-        self.head2 = n - 1
-        self.tail2 = n - 1
+        self.head = 0
+        self.tail = 0
         self.size = 0
 
     def is_empty(self):
@@ -13,42 +11,44 @@ class Deque():
 
     def push_back(self, value):
         if self.size != self.max_n:
-            self.queue[self.head2] = value
-            self.head2 = (self.head2 - 1) % self.max_n
+            self.queue[self.head] = value
+            self.head = (self.head - 1) % self.max_n
             self.size += 1
 
     def push_front(self, value):
         """Готовая функция."""
         if self.size != self.max_n:
-            self.queue[self.tail1] = value
-            self.tail1 = (self.tail1 + 1) % self.max_n
+            self.queue[self.tail] = value
+            self.tail = (self.tail + 1) % self.max_n
             self.size += 1
 
     def pop_front(self):
-        """Готовая функция."""
         if self.is_empty():
             return None
-        x = self.queue[self.head1]
-        self.queue[self.head1] = None
-        self.head1 = (self.head1 + 1) % self.max_n
+        print(self.tail)
+        x = self.queue[self.tail]
+        self.queue[self.tail] = None
+        self.tail = (self.tail - 1) % self.max_n
         self.size -= 1
         print(x)
 
     def pop_back(self):
+        """Готовая функция."""
         if self.is_empty():
             return None
-        x = self.queue[self.tail2]
-        self.queue[self.tail2] = None
-        self.tail2 = (self.tail2 - 1) % self.max_n
+        x = self.queue[self.head]
+        self.queue[self.head] = None
+        self.head = (self.head + 1) % self.max_n
         self.size -= 1
         print(x)
-
 
 # q = Deque(4)
 # q.push_front(861)
 # q.push_front(-819)
+# print(q.queue)
 # q.pop_back()
 # q.pop_back()
+
 
 q = Deque(7)
 q.push_front(-855)
@@ -56,7 +56,7 @@ q.push_front(0)
 q.pop_back()
 q.pop_back()
 q.push_back(844)
-# print(q.queue)
+print(q.queue)
 q.pop_back()
 q.push_back(823)
 print(q.queue)
@@ -89,3 +89,19 @@ print(q.queue)
 # assert q.queue == [None, None, None, None, 11, 0, -1, 1]
 # q.pop_back()
 # assert q.queue == [None, None, None, None, 11, 0, -1, None]
+
+# q = Deque(5)
+# q.push_front(1)
+# q.push_front(2)
+# q.push_front(3)
+# print(q.queue)
+# q.push_back(1)
+# q.push_back(2)
+# q.push_back(3)
+# print(q.queue)
+# q.pop_front()
+# q.pop_front()
+# print(q.queue)
+# q.pop_back()
+# q.pop_back()
+# print(q.queue)
