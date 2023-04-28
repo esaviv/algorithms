ad7d79d0757580c3822ed8e1899d159c80fec0f6
Екатерина Савченко <kattysavchenko1@yandex.ru>
2023-04-28 08:46:27 +0200

    B. Калькулятор. Ревью 1, ОК.

--- 02_basic_data_structures/calculator.py
+++ 02_basic_data_structures/calculator.py
@@ -1,4 +1,4 @@
-# ID успешной посылки - 86532051
+# ID успешной посылки - 86607174
 
 from operator import sub, add, floordiv, mul
 
@@ -7,39 +7,38 @@ class Stack:
     def __init__(self) -> None:
         self.items = []
 
+    def is_empty(self):
+        return len(self.items) == 0
+
     def push(self, item: int) -> None:
         self.items.append(item)
 
     def pop(self) -> int:
+        if self.is_empty():
+            raise IndexError
         return self.items.pop()
 
-    def peek(self) -> int:
-        return self.items[-1]
+
+operators = {
+    '-': sub,
+    '+': add,
+    '/': floordiv,
+    '*': mul
+}
 
 
-def solution(string: str) -> int:
-
-    operators = {
-        '-': sub,
-        '+': add,
-        '/': floordiv,
-        '*': mul
-    }
-
+def get_result(data: str) -> int:
     stack = Stack()
-
-    for element in string:
-        if element.lstrip('-').isdigit():
-            stack.push(int(element))
-        elif element in operators:
-            num1 = stack.pop()
-            num2 = stack.pop()
+    for element in data:
+        if element in operators:
+            num1, num2 = stack.pop(), stack.pop()
             result = operators[element](num2, num1)
             stack.push(result)
-    return stack.peek()
+        else:
+            stack.push(int(element))
+    return stack.pop()
 
 
 if __name__ == '__main__':
     input_data = input().split()
-    # input_data = '4 2 * 4 / 25 * 2 - 12 / 1000 + 2 /'.split()
-    print(solution(input_data))
+    print(get_result(input_data))
